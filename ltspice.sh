fea06f29208f84c177771800a3c46da5c447dcb0
yozu <yasuo@ozu.email>
2017-11-23 00:11:26 +0900

    Added settings file

--- ltspice.sh
+++ ltspice.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
 unset WINEPREFIX
-wine /usr/share/ltspice/XVIIx64.exe
+
+if [ ! -d "${HOME}/.ltspice" ]; then
+	mkdir -p "${HOME}/.ltspice"
+	touch "${HOME}/.ltspice/scad3.ini"
+fi
+
+wine /usr/share/ltspice/XVIIx64.exe -ini "${HOME}/.ltspice/scad3.ini" "$@"
